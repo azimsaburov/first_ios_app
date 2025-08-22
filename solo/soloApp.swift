458a29058a2f2772904b2bb1ec6501574efe7285
Azim <azimsab2000@gmail.com>
2025-08-22 16:58:44 +0600

    Initial Commit

---
+++ solo/soloApp.swift
@@ -0,0 +1,17 @@
+//
+//  soloApp.swift
+//  solo
+//
+//  Created by ZI on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct soloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
