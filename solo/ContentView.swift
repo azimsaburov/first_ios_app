342780018db35fbf6f9ea197fdd96dd1dfe06385
Azim <azimsab2000@gmail.com>
2025-08-23 23:16:31 +0600

    Синтаксис swift и состояние кнопок

--- solo/ContentView.swift
+++ solo/ContentView.swift
@@ -9,62 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var counter = 0
-    var body: some View {
-        VStack(spacing: 20) {
-            //            Image(systemName: "globe")
-            //                .imageScale(.large)
-            //                .foregroundStyle(.tint)
-            Text("Привет Азим")
-                .font(.title)
-                .padding()
-            Text("Счетчик: \(counter)")
-            //.font(.headline)
-                .foregroundColor(.red)
-                .padding()
-                .font(.system(size: 20, weight: .bold))
-                .background(Color.blue.opacity(0.2))
-                .cornerRadius(5)
-            HStack(spacing: 30){            Button(action: {
-                counter += 1
-                print("Кнопка нажата, текушее занчение: \(counter)")}){
-                    Text("Плюс один")
-                }
-                Button(action: {
-                    counter -= 1
-                    print("Кнопка нажата, текушее занчение: \(counter)")}){
-                        Text("Минус один")
-                            .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(20)
-                    }}
-            Button(action: {
-                counter = 0
-                print("Кнопка нажата, текушее занчение: \(counter)")}){
-                    Text("Сброс")
-                        .padding()
-                        .font(.system(size: 20, weight: .bold))
-                        .background(Color.blue.opacity(0.2))
-                        .cornerRadius(5)
-                    
-                }
-            
-            if counter >= 10 {
-                Text("Круто Десяточка!")
-                    .font(.system(size: 30, weight: .medium))
-            }
-            else if counter >= 5 || counter <= -3 {
-                Text("Красава!")
-                    .font(.system(size: 30, weight: .medium))
-            }
-            else {
-                Text("🤔")
-                        .font(.system(size: 30, weight: .medium))
-            }
+    @State private var message = "Welcome!"
+    @State private var flag = true
+    @State private var currentState = 0
 
+    
+    var text: String = "Azim"
+    
+    
+    
+    
+    
+    
+    var body: some View {
+        ZStack{
+            Color.yellow.ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Text(currentText())
+                    .font(.title)
+                Text(message)
+                    .font(.title)
+                    .padding()
+                Text(flag ? "One" : "Two")
+                    .font(.largeTitle)
+                Button( "Нажми меня"){
+                    if flag == true {
+                        message = "Hi"
+                    }else {
+                        message = "Bro"
+                    }
+                    flag.toggle()
+                    currentState = ( currentState + 1) % 4
+                }
+                .padding()
+                .background(Color.white.opacity(0.4))
+                .cornerRadius(100)
+                Text("Счетчик: \(counter)")
+                //.font(.headline)
+                    .foregroundColor(.red)
+                    .padding()
+                    .font(.system(size: 20, weight: .bold))
+                    .background(Color.blue.opacity(0.2))
+                    .cornerRadius(5)
+                HStack(spacing: 30){            Button(action: {
+                    counter += 1
+                    print("Кнопка нажата, текушее занчение: \(counter)")}){
+                        Text("Плюс один")
+                    }
+                    Button(action: {
+                        counter -= 1
+                        print("Кнопка нажата, текушее занчение: \(counter)")}){
+                            Text("Минус один")
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(20)
+                        }}
+                Button(action: {
+                    counter = 0
+                    print("Кнопка нажата, текушее занчение: \(counter)")}){
+                        Text("Сброс")
+                            .padding()
+                            .font(.system(size: 20, weight: .bold))
+                            .background(Color.blue.opacity(0.2))
+                            .cornerRadius(5)
+                        
+                    }
+                
+                if counter == 10 {
+                    Text("Круто Десяточка!")
+                        .font(.system(size: 30, weight: .medium))
+                }
+                else if counter >= 5 || counter <= -3 {
+                    Text("Красава!")
+                        .font(.system(size: 30, weight: .medium))
+                }
+                else {
+                    Text("🤔")
+                        .font(.system(size: 30, weight: .medium))
+                }
+            }
+            .padding()
             
         }
-        .padding()
-            
+    }
+    func currentText() -> String {
+        switch currentState{
+        case 0: "Hello"
+        case 1: "Привет"
+        case 2: "Salam"
+        case 3: "Aloha"
+        default:
+            ""
+        }
     }
 }
 
